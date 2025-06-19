c462f2f9df331f7a86f02ed95937a2df2315b899
Daniel Walz <walzdan@gmail.com>
2025-06-19 17:09:43 -0500

    Base Changes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, About } from './pages';
+import './App.css';
+
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
